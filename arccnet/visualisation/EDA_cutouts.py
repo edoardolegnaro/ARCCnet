bc19d2736bb7c839de3cbf6fcd162c2d9242953e
Edoardo Legnaro <edoardo.legnaro@gmail.com>
2025-01-29 09:51:25 +0100

    deleted EDA notebooks and replaced with .py scripts

---
+++ arccnet/visualisation/EDA_cutouts.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+# ---
+
+# %%
+
+
+import pandas as pd
+
+# Display all columns in the DataFrame
+pd.set_option("display.max_columns", None)
